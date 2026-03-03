3f3d31307b4343157bcfa75e9529cb66f25632aa
thiagof00 <thiagoribeiro0302@gmail.com>
2026-03-03 07:59:21 -0300

    Ajuste de estruturas de interface das paginas home e de registro

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,9 +3,41 @@ function Home() {
 
   return (
     <>
-    <h1>Home</h1>      
-    <Link to="/">Log in</Link>
-    <Link to="/cadastrar">Cadastrar</Link>
+    <div className="top-bar">
+      <button className="more">more</button>
+      <div className="info">
+        <span> Bem vindo, thiago</span>
+        <button>+</button>
+      </div>
+    </div>
+
+    <main>
+      <Link to="/cadastrar">Cadastrar</Link>
+      
+      <div className="listagem">
+        <div className="selecao"> 
+          <button>Civís</button>
+          <button>Brigadistas</button>
+          <button>Internos</button>
+        </div>
+        <table>
+          <tr>
+            <th>Nome</th>
+            <th>CPF</th>
+            <th>Endereço</th>
+            <th>Telefone</th>
+          </tr>
+
+          <tr>
+            <td>Nome</td>
+            <td>CPF</td>
+            <td>Endereço</td>
+            <td>Telefone</td>
+          </tr>
+
+        </table>
+      </div>
+    </main>
 
     </>
   )
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -11,7 +11,6 @@ function Register() {
 
   const onSubmit = (data) => console.log(data)
 
-  console.log(watch("cpf")) // watch input value by passing the name of it
 
 
   return (
@@ -19,14 +18,47 @@ function Register() {
     <h1>Cadastrar</h1>
     <Link to="/home">Voltar</Link>
 
+    <main>
+
     <form onSubmit={handleSubmit(onSubmit)}>
-    <input type="text" {...register("nome", {required: true})} />
+    
+    <label htmlFor="nome">Nome completo</label>
+    <input type="text" {...register("nome", {required: true})} name="nome" />
     {errors.nome && <span>O campo é obrigatório</span>}
 
-    <input type="text" {...register("cpf")} />
+    <label htmlFor="endereco">Endereço</label>
+    <input type="text" {...register("endereco")} name="endereco"/>
+    {errors.endereco && <span>O campo é obrigatório</span>}
+
+    <label htmlFor="email">Email</label>
+    <input type="text" {...register("email")} name="email"/>
+    {errors.email && <span>O campo é obrigatório</span>}
+
+    <label htmlFor="cpf">CPF</label>
+    <input type="text" {...register("cpf")} name="cpf"/>
     {errors.cpf && <span>O campo é obrigatório</span>}
+
+    <label htmlFor="telefone">Telefone</label>
+    <input type="text" {...register("telefone")} name="telefone"/>
+    {errors.endereco && <span>O campo é obrigatório</span>}
+
+    <label htmlFor="perfil">Perfil</label>
+    <select name="perfil" id="perfil" {...register("perfil" , {required: true, value:"CIVIL"})}>
+      <option value="CIVIL">Civil</option>
+      <option value="BRIGADA">Brigadista</option>
+      <option value="INTERNO">Interno</option>
+    </select>
+
+
+
+
+    
     <input type="submit" />
+
+
     </form>
+
+    </main>
     </>
   )
 }
